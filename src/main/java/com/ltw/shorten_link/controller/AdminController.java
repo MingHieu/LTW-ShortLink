984de71c6af67548cdbf398dfeb3bfb1ef9369f8
minhtan <minhtan290902@gmail.com>
2023-04-16 21:23:20 +0700

    fix all

--- src/main/java/com/ltw/shorten_link/controller/AdminController.java
+++ src/main/java/com/ltw/shorten_link/controller/AdminController.java
@@ -1,11 +1,33 @@
 package com.ltw.shorten_link.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ltw.shorten_link.Utils;
+import com.ltw.shorten_link.interfaces.ModelAndViewObject;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class AdminController {
-    
+
+  @GetMapping("/users")
+  public ModelAndView Statistics() {
+    // Authentication authentication =
+    // SecurityContextHolder.getContext().getAuthentication();
+    // String role =
+    // authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).findFirst().get();
+    ModelAndView mv = new ModelAndView("layouts/main");
+    ModelAndViewObject mvo = new ModelAndViewObject("admin/users", "Users", "users.css");
+    mv.addObject("role", "admin");
+    mv.addObject(Utils.ModalAndViewObjectName, mvo);
+    return mv;
+
+  }
+
 }
